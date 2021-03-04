858a29509b8c08e2aea7b336f1bc19966bd2c93c
Benedito Barbosa <200257004@estudantes.ips.pt>
2021-03-04 08:11:05 -0300

    Alteração do nome da classe principal

--- src/main/java/com/agorasystems/ubot/Robot.java
+++ src/main/java/com/agorasystems/ubot/Robot.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Atomic {
+public class Robot {
 
 	private static final String FILENAME = "files/movies.csv";
 
